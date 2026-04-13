dbdcef14a083baf0bae0025928f6496ccf1ad1af
Madina Meiramova <166818321+whoismedi@users.noreply.github.com>
2026-04-13 22:26:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Query
+from fastapi.responses import PlainTextResponse
+import math
+
+app = FastAPI()
+
+def lcm(x:int, y:int) -> int:
+    if x == 0 or y == 0:
+        return 0
+    return abs(x * y) // math.gcd(x,y)
+    
+
+@app.get("/madina_meiramova_2001_gmail_com/lcm", response_class=PlainTextResponse)
+def compute_lcm(x:str=Query(...), y:str=Query(...)):
+    try:
+        x_int = int(x)
+        y_int = int(y)
+        if x_int < 0 or y_int < 0:
+            raise ValueError
+    except (ValueError, TypeError):
+        return "NaN"
+    return str(lcm(x_int, y_int))
+
